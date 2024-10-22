fe380f2eb08153b15ceeca37a5118da7aec4a5a4
gabriel.melatto <gabriel.melatto@shx.com.br>
2024-10-22 14:33:48 -0300

    Adicionando validações no amigo secreto

--- A_partir_do_zero-iniciante_em_programacao/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ A_partir_do_zero-iniciante_em_programacao/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -2,6 +2,17 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+
+    if(amigo.value == '') {
+        alert('Informe o nome do amigo.');
+        return;
+    }
+
+    if(amigos.includes(amigo.value)){
+        alert('Nome já adicionado.');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
 
     amigos.push(amigo.value);
@@ -15,6 +26,11 @@ function adicionar() {
 }
 
 function sortear() {
+    if(amigos.length < 4) {
+        alert('Adicione no mínimo 4 amigos.');
+        return;
+    }
+
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
